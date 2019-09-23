806c34052689bb5823fb681cbea62883b40b6e2c
Bikash <bikash@stridesolution.com.au>
2019-09-23 16:50:18 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        artistQuery: ''
+    };
+
+    updateArtistQuery = (event) => {
+        this.setState({artistQuery: event.target.value});
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <h2>Music Master</h2>
+                <div className="row">
+                    <div className="col-4"></div>
+                    <div className="col-4">
+                        <div className="input-group mb-3">
+                            <input type="text"
+                                   onChange={this.updateArtistQuery}
+                                   className="form-control" placeholder="Search for an Artist"
+                                   aria-label="Search for an Artist" aria-describedby="basic-addon2"/>
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-secondary" type="button">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4"></div>
+                </div>
+                <div>
+                    <p>{this.state.artistQuery}</p>
+                </div>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
